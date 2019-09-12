ad29294610aeff88a84ad1d417f2ee7ca624b1a0
MRxiongyu <247770359@qq.com>
2019-09-12 13:29:46 +0800

    '1'

--- src/App.vue
+++ src/App.vue
@@ -59,6 +59,7 @@
           this.authData.state = getQueryString('state')
           if (!this.authData.authCode && this.payEnv === 'alipay') {
             location.replace(`https://openauth.alipay.com/oauth2/publicAppAuthorize.htm?app_id=2019082366406532&scope=auth_user&state=${this.user.customerId}&redirect_uri=${encodeURI(window.location.href)}`)
+            return
           }
           if (this.authData.authCode && this.authData.state) {
             const params = {
